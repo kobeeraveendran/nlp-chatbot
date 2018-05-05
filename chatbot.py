1fda757cf1729f340d3d6c66f85da1712dfd32c6
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-05-05 09:02:00 -0400

    separated questions from answers

--- chatbot.py
+++ chatbot.py
@@ -19,9 +19,27 @@ for line in lines:
     if len(splitline) == 5:
         id_to_line[splitline[0]] = splitline[4]
 
+# get list of all conversations
 conversation_list = []
 for conversation in conversations[:-1]:
     splitconvos = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", '').replace(' ', '')
     splitbyid = splitconvos.split(',')
     conversation_list.append(splitbyid)
     
+# get questions and answers
+questions = []
+answers = []
+
+# NOTE: check for correctness if current method fails
+'''
+for conversation in conversation_list:
+    for i in range(len(conversation)):
+        if i % 2 == 0:
+            questions.append(id_to_line[conversation[i]])
+        else:
+            answers.append(id_to_line[conversation[i]])
+'''
+for conversation in conversation_list:
+    for i in range(len(conversation)):
+        questions.append(id_to_line[conversation[i]])
+        answers.append(id_to_line[conversation[i + 1]])
