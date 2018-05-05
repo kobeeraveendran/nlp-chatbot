8f9eed6500b8f2c71904a90785c2fcfe79bbce05
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-05-05 08:16:56 -0400

    imported datasets and split columns

--- chatbot.py
+++ chatbot.py
@@ -6,3 +6,7 @@ import numpy as np
 import tensorflow as tf
 import re
 import time
+
+# get the two datasets (conversations and lines)
+lines = open('movie_lines.txt', 'r', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
