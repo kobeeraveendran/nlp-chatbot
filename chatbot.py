0c165be51fd71b03547ef739ec4f0cb8b3f66d4f
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-05-11 23:29:31 -0400

    added RNN's encoding layer

--- chatbot.py
+++ chatbot.py
@@ -178,3 +178,17 @@ def preprocess_targets(targets, word_to_int, batch_size):
     preprocessed_target = tf.concat(values = [left_side, right_side], axis = 1)
 
     return preprocessed_target
+
+# create the RNN's encoding layer
+def encoder_rnn_layer(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
+    lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
+    # deactivates a percentage of neurons: this is usually 20%
+    lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob = keep_prob)
+    encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_dropout] * num_layers)
+    _, encoder_state = tf.nn.bidirectional_dynamic_rnn(cell_fw = encoder_cell, 
+                                                       cell_bw = encoder_cell, 
+                                                       sequence_length = sequence_length, 
+                                                       inputs = rnn_inputs, 
+                                                       dtype = tf.float32)
+    
+    return encoder_state
